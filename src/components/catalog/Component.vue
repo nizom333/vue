472286273dbf324a292514428ea06eedfdb88bda
WIN-GV0D9LFM58E\Nizom <nizomchik7207@mail.ru>
2019-03-21 22:40:20 +0500

    commit

---
+++ src/components/catalog/Component.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        {{mess}}
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            mess: 'Nizomiddin',
+        }
+    }
+}
+</script>
